ca75f128027258c825a9080b2e216bc7f593aa40
Yurii Tyshchenko <55381248+gravityimp@users.noreply.github.com>
2023-12-05 14:14:51 +0100

    Update CirclePart.swift

--- MemoryGame/CirclePart.swift
+++ MemoryGame/CirclePart.swift
@@ -16,12 +16,24 @@ struct CirclePart: Shape {
     
     func path(in rect: CGRect) -> Path {
         let center = CGPoint(x: rect.width / 2, y: rect.height / 2)
-        let radius = min(rect.width / 2, rect.height / 2)
+        let radius = min(rect.width, rect.height) / 2
+        let start = CGPoint(
+            x: center.x + radius * CGFloat(cos(startAngle.radians)),
+            y: center.y + radius * CGFloat(sin(startAngle.radians))
+        )
+        
         var circlePath: Path = Path()
         
         circlePath.move(to: center)
-        circlePath.addArc(center: center, radius: radius, startAngle: angleStart, endAngle: angleEnd, clockwise: false)
-        circlePath.closeSubpath()
+        circlePath.addLine(to: start)
+        circlePath.addArc(
+            center: center, 
+            radius: radius, 
+            startAngle: angleStart, 
+            endAngle: angleEnd, 
+            clockwise: false
+        )
+        circlePath.addLine(to: center)
         return circlePath
     }
 }
